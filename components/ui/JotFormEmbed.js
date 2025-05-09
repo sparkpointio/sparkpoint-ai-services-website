0187c2e23b15b247fc42fc12586c00dc57ba6f98
Harvey Javier <harveyjavier17@gmail.com>
2025-05-09 09:59:53 +0800

    Remove unused var, `useEffect` on `components/ui/JotFormEmbed.js`

--- components/ui/JotFormEmbed.js
+++ components/ui/JotFormEmbed.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export default function JotFormModal() {
     const [isOpen, setIsOpen] = useState(false);
